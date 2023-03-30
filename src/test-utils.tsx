988d27e695d6bb55d748a3d22b801ed75b6b7ff0
allpha23 <allpha23@gmail.com>
2023-03-30 15:26:52 -0300

    chore: configurações iniciais do projeto

---
+++ src/test-utils.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React, { FC, ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => (
+  <BrowserRouter>{children}</BrowserRouter>
+);
+
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
